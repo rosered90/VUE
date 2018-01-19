50bf41567ff1ebb9cf748a454bdf53d84322dcfd
rose_redff <1404248584@qq.com>
2018-01-19 20:52:31 +0800

    Initial commit

---
+++ static/js/server.js
@@ -0,0 +1,17 @@
+//使用require指令载入http模块，并将实力胡阿德HTTP赋值给变量http
+var http = require('http');
+
+
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
